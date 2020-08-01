8d96a965cab0486631db43bff283bee436aa6416
lucas-leme <lucaslssantos99@gmail.com>
2020-08-01 12:46:03 -0300

    Add text files

--- bot.py
+++ bot.py
@@ -1,40 +1,59 @@
-import time
+# Funções do Alan
 import telebot
+# Insira o token
 import os
+Token = os.environ['TELEGRAM_TOKEN']
 
-TOKEN = os.environ['TELEGRAM_TOKEN']
-bot = telebot.TeleBot(token=TOKEN)
+bot = telebot.TeleBot(Token)
 
-def findat(msg):
-    # from a list of texts, it finds the one with the '@' sign
-    for i in msg:
-        if '@' in i:
-            return i
-
-@bot.message_handler(commands=['start']) # welcome message handler
+@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['help'])
 def send_welcome(message):
-    bot.reply_to(message, '(placeholder text)')
+    bot.reply_to(message, "Comandos: /quant; /nlp; /cv; /rl; /ds")
 
-@bot.message_handler(commands=['help']) # help message handler
+
+# Descrição das Areas de foco
+
+#quant
+@bot.message_handler(commands=['quant'])
+def send_quant_describe(message):
+    text_quant = open("./text_files/quant_describe.txt", "r")
+    bot.reply_to(message, text_quant)
+
+#nlp
+@bot.message_handler(commands=['nlp'])
+def send_nlp_describe(message):
+    text_nlp = open("./text_files/nlp_describe.txt", "r")
+    bot.reply_to(message, text_nlp)
+
+#cv
+@bot.message_handler(commands=['cv'])
+def send_cv_describe(message):
+    text_cv = open("./text_files/cv_describe.txt", "r")
+    bot.reply_to(message, text_cv)
+
+#rl
+@bot.message_handler(commands=['rl'])
+def send_rl_describe(message):
+    text_rl = open("./text_files/rl_describe.txt", "r")
+    bot.reply_to(message, text_rl)
+
+#ds
+@bot.message_handler(commands=['ds'])
+def send_ds_describe(message):
+    text_ds = open("./text_files/ds_describe.txt", "r")
+    bot.reply_to(message, text_ds)
+
+
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, 'ALPHA = FEATURES MAY NOT WORK')
+    bot.reply_to(message, "Salve, Salve Grupo Turing!")
 
-@bot.message_handler(func=lambda msg: msg.text is not None and '@' in msg.text)
-# lambda function finds messages with the '@' sign in them
-# in case msg.text doesn't exist, the handler doesn't process it
-def at_converter(message):
-    texts = message.text.split()
-    at_text = findat(texts)
-    if at_text == '@': # in case it's just the '@', skip
-        pass
-    else:
-        insta_link = "https://instagram.com/{}".format(at_text[1:])
-        bot.reply_to(message, insta_link)
-
-while True:
-    try:
-        bot.polling(none_stop=True)
-        # ConnectionError and ReadTimeout because of possible timout of the requests library
-        # maybe there are others, therefore Exception
-    except Exception:
-        time.sleep(15)
+@bot.message_handler(regexp="Pistola")
+def handle_message(message):
+    bot.reply_to(message, "Vc quis dizer Azank??")
+    
+@bot.message_handler(content_types=['document'])
+def handle_docs_audio(message):
+    bot.reply_to(message, "Oloko mandou a braba")
+bot.polling()
