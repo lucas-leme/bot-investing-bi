0eb78e9ca54a3dea0d208b7875e078f5b1eb44c6
lucas-leme <lucaslssantos99@gmail.com>
2020-09-13 19:48:12 -0300

    Remove txt folder

--- bot.py
+++ bot.py
@@ -33,31 +33,26 @@ def send_welcome(message):
 #quant
 @bot.message_handler(commands=['quant'])
 def send_quant_describe(message):
-    #text_quant = open("./text_files/quant_describe.txt", "r")
     bot.reply_to(message, 'Essa área de foco tem como principal objetivo estudar as aplicações de programação no mercado financeiro, através de determinadas plataformas. Buscamos, juntos, aprender tanto sobre mercado financeiro quanto sobre aplicações e métodos quantitativos utilizados nesse mercado.')
 
 #nlp
 @bot.message_handler(commands=['nlp'])
 def send_nlp_describe(message):
-    #text_nlp = open("./text_files/nlp_describe.txt", "r")
     bot.reply_to(message, 'Processamento de Linguagem Natural é uma área da inteligência artificial cujo objetivo é a interpretação e manipulação de linguagens humanas. NLP tem muitas tarefas, algumas se relacionam ao processamento mais imediato dos componentes linguísticos, como a análise sintática, morfossintática (POS Tagging), lematização etc.')
 
 #cv
 @bot.message_handler(commands=['cv'])
 def send_cv_describe(message):
-    #text_cv = open("./text_files/cv_describe.txt", "r")
     bot.reply_to(message, 'Em Computer Vision (ou Visão Computacional) trabalhamos principalmente com o processamento de imagens.')
 
 #rl
 @bot.message_handler(commands=['rl'])
 def send_rl_describe(message):
-    #text_rl = open("./text_files/rl_describe.txt", "r")
     bot.reply_to(message, 'O Aprendizado por Reforço é uma das áreas mais únicas do Aprendizado de Máquina, fundamentada em ensinar a um agente como agir em um ambiente a partir de suas experiências.')
 
 #ds
 @bot.message_handler(commands=['ds'])
 def send_ds_describe(message):
-    #text_ds = open("./text_files/ds_describe.txt", "r")
     bot.reply_to(message, 'Data Science (ou Ciência de Dados) é sobre obter insights ou conhecimento através do estudo e análise de dados')
 
 
