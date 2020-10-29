72f75f1f4ae60367a4cccab373f0f5710702caa6
Lucas Leme <lucaslssantos99@gmail.com>
2020-10-29 00:16:16 -0300

    Fix stocks names

--- investing.py
+++ investing.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def get_investiments_returns(pct_change_window=1):
 
-    stocks_name = ['bcff11', 'gogl34, aapl34']
+    stocks_name = ['bcff11', 'gogl34', 'aapl34']
     funds_name = ['Visia Zarathustra Fundo De Investimento Em Cotas De Fundo De Investimento Multimercado',
                   'Arx Fundo De Investimento Em Ações',
                   'Alaska Black Fundo De Investimento Em Cotas De Fundos De Investimento Em Ações Ii - Bdr Nível I',
