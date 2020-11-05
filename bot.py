6b2609fef0b43cb6873861ae50709fa78e41c224
Lucas Leme <lucaslssantos99@gmail.com>
2020-11-05 11:06:07 -0300

    Fix reply photo params

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ def last_returns(update, context):
         window = int(update.message.text[8:])
         text = get_investiments_last_period_performace(window)
         update.message.reply_text(text)  
-        update.message.reply_photo(update.message.chat_id, 'image.png')
+        update.message.reply_photo(photo = 'image.png')
 
         os.remove('image.png')
     except Exception as e:
